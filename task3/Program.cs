5b841ce8b313ac4df8ea0df4f632637a408beadb
Dasha <dashabutenko@list.ru>
2022-12-14 22:57:22 +0400

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new int[5,4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.Write($"\n");
+}
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i,j];
+    }
+    Console.Write($"Ср.арифметическое {j} столбца = {Math.Round((double)sum / array.GetLength(0), 1)}\n");
+}
