f95e1eaecbdb966341d50354ac69337c86b1df11
Dasha <dashabutenko@list.ru>
2022-12-14 21:53:57 +0400

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+int m, n;
+Console.Write($"Введите количество строк в массиве: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.Write($"Введите количество столбцов в массиве: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+double[,] array = new double[m,n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(100) / 10.0;
+        Console.Write($"{array[i, j]}     ");
+    }
+    Console.Write($"\n");
+}
+Console.Write($"\n");
